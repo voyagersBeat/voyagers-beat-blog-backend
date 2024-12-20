f36abc2e05fe63c51de6bf1c021df31e33560afe
priyanshu-inu <priyanshuinuu@gmail.com>
2024-12-20 13:14:57 +0530

    newest-3

--- src/middleware/generate.token.js
+++ src/middleware/generate.token.js
@@ -4,7 +4,6 @@ const JWT_SECRET = process.env.jwt_SECRET_KEY;
 
 const generateToken = async (userId) => {
   try {
-    // Check if JWT_SECRET is available
     if (!JWT_SECRET) {
       throw new Error("JWT_SECRET_KEY is not set in environment variables.");
     }
@@ -14,16 +13,20 @@ const generateToken = async (userId) => {
       throw new Error("User not found");
     }
 
-    // Create JWT token
     const token = jwt.sign({ userId: user._id, role: user.role }, JWT_SECRET, {
-      expiresIn: "1h", // Set token expiry as 1 hour
+      expiresIn: "1h", // Token expiration
+    });
+
+    // Set the token in a cookie
+    res.cookie("token", token, {
+      httpOnly: true, // Prevents client-side JavaScript from accessing the cookie
+      secure: process.env.NODE_ENV === "production", // Secure flag for HTTPS
+      maxAge: 3600000, // Cookie expiration (1 hour)
     });
 
     return token;
   } catch (err) {
-    console.log("Error to generate token:", err);
+    console.log("Error generating token:", err);
     throw err;
   }
 };
-
-module.exports = generateToken;
