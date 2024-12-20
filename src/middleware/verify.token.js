91ecd2a04c01d496585c0e0e9add82097e314112
priyanshu-inu <priyanshuinuu@gmail.com>
2024-12-20 15:49:04 +0530

    newest-5

--- src/middleware/verify.token.js
+++ src/middleware/verify.token.js
@@ -3,7 +3,7 @@ const JWT_SECRET = process.env.jwt_SECRET_KEY;
 
 const verifyToken = (req, res, next) => {
   try {
-    const token = req.cookies.token;
+    const token = req.Cookies.token;
 
     if (!token) {
       return res.status(401).send({ message: "No token provided" });
